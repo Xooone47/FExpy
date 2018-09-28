5c80054d667f507601a9267121e2f039536593ef
sinkpear <372653853@qq.com>
2018-09-28 20:23:06 +0800

    webpack配置优化

--- webpack.config.js
+++ webpack.config.js
@@ -5,101 +5,105 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const autoprefixer = require('autoprefixer');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist', 'assets'),
-        publicPath: '/assets/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json'],
-        modules: ['node_modules']
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 4747,
-        inline: true,
-        historyApiFallback: {
-            rewrites: [
+module.exports = (env, argv) => {
+    const mode = argv.mode === 'development' ? 'DEV' : 'PROD';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'index.[hash].js',
+            path: path.resolve(__dirname, 'dist', 'assets'),
+            publicPath: '/assets/'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.json'],
+            modules: ['node_modules']
+        },
+        devtool: 'inline-source-map',
+        devServer: {
+            publicPath: '/assets/',
+            port: 4747,
+            inline: true,
+            historyApiFallback: {
+                index: '/assets/',
+                rewrites: [
+                    {
+                        from: /^[^/rest/v1].*$/,
+                        to: 'index.html'
+                    }
+                ]
+            },
+            // proxy: {
+            //     '/rest': {
+            //         target: 'http://',
+            //         secure: false
+            //     }
+            // },
+            hot: true
+        },
+        module: {
+            rules: [
                 {
-                    from: /^[^/rest/v1].*$/,
-                    to: '/index.html'
+                    test: /(\.jsx|\.js)$/,
+                    use: [
+                        "babel-loader",
+                        {
+                            loader: 'eslint-loader',
+                            options: {
+                                // cache: true,
+                                configFile: path.join(__dirname, '.eslintrc.json')
+                            }
+                        }
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.less$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    importLoaders: 2,
+                                    modules: true,
+                                    camelCase: 'dashes',
+                                    localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                                }
+                            },
+                            'less-loader', 'postcss-loader']
+                    })
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader'
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                      'file-loader'
+                    ]
                 }
             ]
         },
-        // proxy: {
-        //     '/rest': {
-        //         target: 'http://',
-        //         secure: false
-        //     }
-        // },
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: [
-                    "babel-loader",
-                    {
-                        loader: 'eslint-loader',
-                        options: {
-                            // cache: true,
-                            configFile: path.join(__dirname, '.eslintrc.json')
-                        }
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 2,
-                                modules: true,
-                                camelCase: 'dashes',
-                                localIdentName: '[path][name]__[local]--[hash:base64:5]',
-                            }
-                        },
-                        'less-loader', 'postcss-loader']
-                })
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                  'file-loader'
-                ]
-            }
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'FExpy',
+                template: "./src/index.html",
+                filename: mode === 'DEV' ? 'index.html' : '../index.html'
+            }),
+            new ExtractTextPlugin('styles.[hash].css'),
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'FExpy',
-            hash: true,
-            template: "./src/index.html",
-            filename: '../index.html'
-        }),
-        new ExtractTextPlugin('style.css'),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+    };
 };
