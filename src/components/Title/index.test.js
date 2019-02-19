5a3afa9c06404cd26e726e3656399d96dade224e
sinkpear <372653853@qq.com>
2019-02-19 20:47:56 +0800

    add husky

--- src/components/Title/index.test.js
+++ src/components/Title/index.test.js
@@ -1,25 +1,10 @@
 import {shallow, render, mount} from 'enzyme';
 import Title from './index';
 
-export const setup = component => {
-    const wrapper = shallow(component);
-    return wrapper;
-};
-
-export const setupByRender = component => {
-    const wrapper = render(component);
-    return wrapper;
-};
-
-export const setupByMount = component => {
-    const wrapper = mount(component);
-    return wrapper;
-};
-
 const props = {};
 
 describe('test title', () => {
-    const wrapper = setup(<Title {...props} />);
+    const wrapper = shallow(<Title {...props} />);
 
     it('has a title named FExpy', () => {
         expect(wrapper.find('h1').text()).toBe('FExpy');
