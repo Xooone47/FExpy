f49a71f245eed9bce88b2d9d71fc8df9211ae938
Deland <insidethe47s@qq.com>
2022-07-05 16:04:26 +0800

    feat: jira generator

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {RecoilRoot} from 'recoil';
 import {store} from '@/store';
+import JiraGenerator from '@/components/Home/JiraGenerator';
 import {Home} from '..';
 import styles from './styles.less';
 
@@ -12,6 +13,7 @@ const App = () => (
                 <div className={styles.root}>
                     <Switch>
                         <Route path="/" exact component={Home} />
+                        <Route path="/jira-generator" exact component={JiraGenerator} />
                         <Redirect from="*" to="/" />
                     </Switch>
                 </div>
